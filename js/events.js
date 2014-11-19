9d8d94091d63df97a0be1a450e9b77d6edeaba7c
Peter Krieg <copperkrieg@gmail.com>
2014-11-19 04:16:05 -0500

    practiced event listeners with html/js from scratch

---
+++ js/events.js
@@ -0,0 +1,23 @@
+
+window.onload = function(){
+
+	var canvas = document.getElementById('canvas');
+	canvas.addEventListener('click', function(event){
+		console.log('it hs ben clicked');
+	}, false);
+
+
+
+	var button = document.getElementById('button');
+	button.addEventListener('click', function(event){
+		console.log('button clicked!!!!!');
+	}, false);
+
+
+
+
+
+}
+
+
+
