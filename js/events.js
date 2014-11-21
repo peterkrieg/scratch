b6fcdc0e9adab0df385c06741312c6129f43b090
Peter Krieg <copperkrieg@gmail.com>
2014-11-20 22:56:05 -0500

    played around with more JS events for practice

--- js/events.js
+++ js/events.js
@@ -9,9 +9,51 @@ window.onload = function(){
 
 
 	var button = document.getElementById('button');
+
+	button.addEventListener('mousedown', function(event){
+		console.log('button mousedown!!!!!');
+	}, false);
+
+
+
+
+	button.addEventListener('mouseup', function(event){
+		console.log('button mouseup!!!!!');
+	}, false);
 	button.addEventListener('click', function(event){
 		console.log('button clicked!!!!!');
 	}, false);
+
+
+
+
+
+
+
+
+
+	function onKeyboardEvent (event) {
+		function onKeyboardEvent (event) {       
+			switch (event.keyCode) { 
+				case keycode.UP:         
+				console.log("up!");         
+				break; 
+				case keycode.DOWN:         
+				console.log("down!");         
+				break; 
+				case keycode.LEFT:         
+				console.log("left!");         
+				break; 
+				case keycode.RIGHT:         
+				console.log("right!");         
+				break;       
+				default:         
+				console.log(event.keyCode);       
+			}     
+		}    
+		window.addEventListener('keydown', onKeyboardEvent, false);   
+	};
+	
 
 
 
