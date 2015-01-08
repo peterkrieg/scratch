6a7aaf57dd60c786250d59ada9adfec2d4ffe991
Peter Krieg <copperkrieg@gmail.com>
2015-01-07 19:57:52 -0500

    practice basic JS from book

---
+++ js/js1.js
@@ -0,0 +1,13 @@
+document.write('hello world');
+
+var newintro = 'hat what what TESTTEST';
+var intro = document.getElementById('intro');
+intro.innerHTML= newintro;
+
+document.write('\n screen width is ' + window.screen.width);
+
+document.write('\n test');
+
+document.write('\n ' + document.lastModified);
+
+document.write(Math.PI);
