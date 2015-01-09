a5ee29c63fa080fb0dedd2d5056029333549adce
Peter Krieg <copperkrieg@gmail.com>
2015-01-09 14:46:07 -0500

    played around with DOM

--- js/js1.js
+++ js/js1.js
@@ -4,10 +4,19 @@ var newintro = 'hat what what TESTTEST';
 var intro = document.getElementById('intro');
 intro.innerHTML= newintro;
 
-document.write('\n screen width is ' + window.screen.width);
+document.write('\n screen width is ' + window.screen.width );
 
 document.write('\n test');
 
 document.write('\n ' + document.lastModified);
 
 document.write(Math.PI);
+
+console.log(window.screenY);
+console.log(window.screenX);
+
+
+var msg = '<p> hello world this is a test!!  </p>';
+var el = document.getElementById('divtest');
+el.innerHTML = msg;
+
