75e43f403df1441b9e2b14b6a4b55cd8f49931f2
Peter Krieg <copperkrieg@gmail.com>
2015-01-10 01:28:39 -0500

    finished chap 4

--- js/js1.js
+++ js/js1.js
@@ -16,7 +16,33 @@ console.log(window.screenY);
 console.log(window.screenX);
 
 
-var msg = '<p> hello world this is a test!!  </p>';
+var msg = '<p class="center"> hello world this is a test!!  </p>';
 var el = document.getElementById('divtest');
 el.innerHTML = msg;
+
+
+var dow = new Date();
+
+alert('the day of week is ' + dow.getDate());
+
+// var month = dow.getMonth();
+// switch(month){
+// 	case 0:
+// 		month='January';
+// 		break;
+// 	case 1:
+// 		month='February';
+// 		break;
+// 	default:
+// 		month='December';
+//		break;
+// }
+
+// alert('the month name is ' + month);
+
+var month = dow.getMonth();
+var months = ['January', 'February', 'March', 'April', 'May', 
+							'June', 'July', 'August', 'September', 'Oct', 'Nov', 'Dec'];
+
+alert('the day of week isss ' + months[month]);
 
