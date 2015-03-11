f7153ab82c7f2d2875a7c25434f3ddf9998f64b6
Peter Krieg <copperkrieg@gmail.com>
2015-03-10 23:23:54 -0400

    kept struggling to make skroll js and sass/skroll stylesheets work

---
+++ js/js8.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	$('body').hide().fadeIn(500);
+	// This initiates the skroll js library
+	var s = skrollr.init();
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
