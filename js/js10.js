7accfc7b59bcc60d6e930d45c10d2d5483b0094a
Peter Krieg <pkrieg2@gmail.com>
2015-04-16 10:59:50 -0400

    added a basic pin example with ScrollMagic

--- js/js10.js
+++ js/js10.js
@@ -9,17 +9,41 @@ TweenMax.staggerTo(".box", 3, {
 	rotation: 360,
 	scale: 1.5,
 	ease: Bounce.easeOut,
+	// background: 'rgb(255, 0, 0)',
 	delay: .5
 },
 .5
 );
 
 TweenMax.staggerFrom(".box", 3, {
-	delay: .5,
-	opacity: 0
+	// delay: .5,
+	// opacity: 0
 })
 
 
 
 
+
+
+// Starting to work with ScrollMagic for first time
+
+var controller = new ScrollMagic.Controller();
+
+// creating a scene
+var scene = new ScrollMagic.Scene({
+	triggerElement: '.triggerLine',
+	duration: 500
+})
+.setPin('.box2')
+
+controller.addScene(scene);
+
+
+
+
+
+
 });
+
+
+
