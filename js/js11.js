b7f4455a838fcd87aaa3d3208e73cd4368acdb09
Peter Krieg <pkrieg2@gmail.com>
2015-04-16 13:10:58 -0400

    cant get scrollmagic to work, going to try new index file from template source example

--- js/js11.js
+++ js/js11.js
@@ -2,8 +2,25 @@ $(function(){
 	
 
 
+// Simple GSAP for the boxes 
+
+TweenMax.staggerTo('.box', 1, {
+	x: 200,
+	scale: .6,
+	rotation: 90
+},
+.3
+)
 
 
 
+// Trying to work Scrollmagic on box2
 
+var controller = new ScrollMagic.Controller();
+
+var scene = new ScrollMagic.Scene({
+	triggerElement: ".triggerLine"
+})
+	.setTween(".box2", 0.5, {backgroundColor: "green", scale: 2.5}) // trigger a TweenMax.to tween
+	.addTo(controller);
 });
