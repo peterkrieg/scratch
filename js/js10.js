9a04d01708cd3ba01442125abf6ee7bc0c9c90a6
Peter Krieg <pkrieg2@gmail.com>
2015-04-15 23:46:18 -0400

    added GSAP animation and played around with it, very cool..

--- js/js10.js
+++ js/js10.js
@@ -2,6 +2,23 @@ $(document).ready(function(){
 	
 
 
+// GreenSock Animation Platform
+
+TweenMax.staggerTo(".box", 3, {
+	x: 435,
+	rotation: 360,
+	scale: 1.5,
+	ease: Bounce.easeOut,
+	delay: .5
+},
+.5
+);
+
+TweenMax.staggerFrom(".box", 3, {
+	delay: .5,
+	opacity: 0
+})
+
 
 
 
