d56b27c335c131d80c2b959a7d858d67e1b78d10
Peter Krieg <pkrieg2@gmail.com>
2015-09-22 00:56:49 -0400

    fixed small error, to make first example of very basic AJAX. just loading HTML from different sources, using jQuery .load() method

--- morepractice/3/js.js
+++ morepractice/3/js.js
@@ -4,11 +4,11 @@ var listItem = $('li');
 
 listItem.on('click', function(e){
 	e.preventDefault();
-	var url = $(this).prop('data-link');
+	var url = $(this).attr('data-link');
 
 	$('li.current').removeClass('current');
 	$(this).addClass('current');
-
 	$('div.container').empty();
 	$('div.container').load(url + ' div.content').hide().fadeIn(1500);
 });
+
