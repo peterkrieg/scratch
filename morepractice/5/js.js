6b719c64135ec1f954e14000576ebd3f825972f8
Peter Krieg <pkrieg2@gmail.com>
2015-09-24 23:57:58 -0400

    more practice and trial with angular.  cant figure out controllers, error "ng:areq" very basic stuff..

--- morepractice/5/js.js
+++ morepractice/5/js.js
@@ -1,6 +1,14 @@
 
-function Form($scope){
-	$scope.name = 'Peter Krieg';
-	$scope.age = 22;
-	$scope.alive = true;
-}
+// function Form($scope){
+// 	$scope.name = 'Peter Krieg';
+// 	$scope.age = 22;
+// 	$scope.alive = true;
+
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('AnotherForm', ['$scope', function($scope) {
+	$scope.description = 'I am 22 and enjoy outdoors';
+}]);
+
+
